19e9a7bbe50956bf79ddc54d7f2aa6ecffcc8cea
Dennis Chua <dmcvocation@gmail.com>
2024-12-25 12:50:42 -0600

    Updated root/src/main.rs to elaborate on the nature of this package.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
 fn main() {
-    println!("\nHello! Welcome to my Rust-Raylib workbook.\n");
+    println!("\nHello! Welcome to my Rust-Raylib package.");
+	println!("Herein you'll find the following workspaces:");
+	println!("\t- raylib-intro-course\n");
 }
