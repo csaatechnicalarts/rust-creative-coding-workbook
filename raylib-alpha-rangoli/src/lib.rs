70e6ff7c708c26e413d7736589c7948aac6ce8fd
Dennis Chua <dmcvocation@gmail.com>
2024-12-30 12:57:35 -0600

    Refactor code to a library crate.

---
+++ raylib-alpha-rangoli/src/lib.rs
@@ -0,0 +1,50 @@
+#![allow(warnings)]
+
+pub static LOWER_BOUND: i32 = 1;
+pub static UPPER_BOUND: i32 = 26;
+
+pub fn print_rangoli(n: i32) {
+    let (rangoli_output, max_width) = generate_rangoli_blob(n);
+
+    for line in rangoli_output.into_iter().rev() {
+        println!("{:-^width$}", line, width = max_width as usize);
+    }
+}
+
+fn generate_rangoli_blob(n: i32) -> (Vec<String>, i32) {
+    let mut rangoli_lines: Vec<String> = vec![];
+    for i in (-1..=(n - 2)).rev() {
+        rangoli_lines.push(print_line(n - 1, i));
+    }
+
+    let max_width = rangoli_lines.last().unwrap().len();
+
+    for line in &rangoli_lines {
+        println!("{:-^width$}", line, width = max_width);
+    }
+
+    let rangoli_output = &rangoli_lines[..(rangoli_lines.len() - 1)];
+
+    (rangoli_lines, max_width as i32)
+}
+
+fn print_line(n: i32, m: i32) -> String {
+    let ascii_lower: [char; UPPER_BOUND as usize] = [
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+        's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    ];
+
+    let mut r_line: Vec<String> = vec![];
+
+    let mut i = n;
+
+    while i > m {
+        r_line.push(ascii_lower[i as usize].to_string());
+        i -= 1;
+    }
+
+    for j in (i + 2)..(n + 1) {
+        r_line.push(ascii_lower[j as usize].to_string());
+    }
+    r_line.join("-")
+}
--- raylib-alpha-rangoli/src/main.rs
+++ raylib-alpha-rangoli/src/main.rs
@@ -8,50 +8,8 @@ struct CliParam {
     number: i32,
 }
 
-static LOWER_BOUND: i32 = 1;
-static UPPER_BOUND: i32 = 26;
-
-fn print_line(n: i32, m: i32) -> String {
-    let ascii_lower: [char; UPPER_BOUND as usize] = [
-        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
-        's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-    ];
-
-    let mut r_line: Vec<String> = vec![];
-
-    let mut i = n;
-
-    while i > m {
-        r_line.push(ascii_lower[i as usize].to_string());
-        i -= 1;
-    }
-
-    for j in (i + 2)..(n + 1) {
-        r_line.push(ascii_lower[j as usize].to_string());
-    }
-    r_line.join("-")
-}
-
-fn print_rangoli(n: i32) {
-    let mut rangoli_lines: Vec<String> = vec![];
-    for i in (-1..=(n - 2)).rev() {
-        rangoli_lines.push(print_line(n - 1, i));
-    }
-
-    let max_width = rangoli_lines.last().unwrap().len();
-
-    for line in &rangoli_lines {
-        println!("{:-^width$}", line, width = max_width);
-    }
-
-    let rangoli_output = &rangoli_lines[..(rangoli_lines.len() - 1)];
-
-    for line in rangoli_output.into_iter().rev() {
-        println!("{:-^width$}", line, width = max_width);
-    }
-}
-
 fn main() {
+    use raylib_alpha_rangoli::{print_rangoli, LOWER_BOUND, UPPER_BOUND};
     use std::process;
 
     let cli_param = CliParam::parse();
