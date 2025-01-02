eb8c8cf363609481bdeb44f0c69a57c121b4be26
Dennis Chua <dmcvocation@gmail.com>
2025-01-02 11:21:18 -0600

    Refactor Raylib setup, logic and draw code out of main.rs into raylib_mod.rs. Put in RLDriver fields in preparation for calculating the draw coordinates of each alphabet in every line of the rangoli pattern.
    TO-DO in the RLDriver::build() method:
    
    [1] Given the font set, create the lookup table, alpha_pos, by
    calculating the x- and y-offset of 26 letter-glyph in the alphabet.
    
    [2] Given the rangoli pattern and alpha_pos, find a way to calculate the
    screen x- and y-coordinates of every letter in every line of the rangoli
    pattern. As this is a static data set up only once, get this done before
    the draw logic loop.

--- raylib-alpha-rangoli/src/main.rs
+++ raylib-alpha-rangoli/src/main.rs
@@ -14,7 +14,7 @@ struct CliParam {
 fn main() {
     use std::process;
     use raylib::prelude::*;
-    use crate::raylib_mod::{SCREEN_HEIGHT, SCREEN_WIDTH, DEFAULT_FPS, RLDriver};
+    use crate::raylib_mod::RLDriver;
     use crate::rangoli::{print_rangoli, generate_rangoli_pattern, LOWER_BOUND, UPPER_BOUND};
 
     // *************************
@@ -35,62 +35,10 @@ fn main() {
     let (rangoli_pattern , max_size) = generate_rangoli_pattern(cli_param.number);
     //println!("{:#?}", rangoli_pattern);
 
-
-    // ********************
-    // Raylib set up block.
-    // ********************
-
-
-    let (mut rl, thread) = raylib::init()
-        .size(SCREEN_WIDTH, SCREEN_HEIGHT)
-        .title("Rust Alphabet Rangoli (ver. 0.9)")
-        .build();
-
-    rl.set_target_fps(DEFAULT_FPS);
-
-    // Raylib-Rust calls Raylib-C using FFI. When font loading fails, the following Rust code
-    // does not print the expect() message then panic. Instead, internally the C-library falls
-    // back on its default font, after printing a warning message. See rtext.c LoadBMFont() 
-    // TRACELOG message.
- 
-    let font = rl.load_font(&thread, "resources/romulus.png").expect("Couldn't load font!");
-    println!("\n{:#?}", font);
-
-    //let rld = RLDriver::build(&mut rl, &thread, DEFAULT_FPS, &font, &rangoli_pattern);
-
-    let mut rangoli_pos = vec![Vector2::default(); rangoli_pattern.len()];
-
-    for i in 0..rangoli_pattern.len() {
-        rangoli_pos[i].x = (SCREEN_WIDTH as f32 / 2.0) - (rl.measure_text(&rangoli_pattern[i], 20)/2) as f32;
-        rangoli_pos[i].y = 60.0 + (15.0 * (i as f32));
-    }
-    //println!("\n{:#?}", rangoli_pos);
-
-    while !rl.window_should_close() {
-
-        // *******************
-        // Raylib logic block.
-        // *******************
-
-        // ******************
-        // Raylib draw block.
-        // ******************
-
-        let mut d = rl.begin_drawing(&thread);
-
-        d.clear_background(Color::GAINSBORO);
-
-        for i in 0..rangoli_pattern.len() {
-            let r_line = rangoli_pattern.get(i).unwrap();
-            d.draw_text_ex(
-                &font, 
-                r_line, 
-                rangoli_pos[i as usize],
-                20.0, 
-                1.0, 
-                Color::GRAY
-            );
-        }
-    }
+    // ****************************
+    // Raylib set up and call block.
+    // ****************************
+    let mut rld = RLDriver::build(String::from("resources/alpha_beta.png"), &rangoli_pattern);
+    rld.run();
 }
 
--- raylib-alpha-rangoli/src/raylib_mod.rs
+++ raylib-alpha-rangoli/src/raylib_mod.rs
@@ -2,9 +2,10 @@
 
 use raylib::prelude::*;
 
-pub const SCREEN_WIDTH: i32 = 800;
-pub const SCREEN_HEIGHT: i32 = 450;
-pub const DEFAULT_FPS: u32 = 24;
+const SCREEN_WIDTH: i32 = 800;
+const SCREEN_HEIGHT: i32 = 450;
+const DEFAULT_FPS: u32 = 24;
+const FONT_SIZE: i32 = 20;
 
 pub struct RLDriver<'pattern_lt> {
     rl: RaylibHandle,
@@ -12,43 +13,57 @@ pub struct RLDriver<'pattern_lt> {
     fps: u32,
     font: Font,
     // Generated text pattern owned by the rangoli module.
-    rangoli_pattern: &'pattern_lt Vec<String>,     
+    rangoli_pattern: &'pattern_lt Vec<String>,
+    // Draw call x-y coordinate for the rangoli pattern.
+    rangoli_pos: Vec<Vector2>,
     // Relative x- and y-offset of each English alphabet character based on the font set.
-    alpha_pos: [Vector2; 26],  
+    alpha_offset: [Vector2; 26],
 }
 
 impl<'pattern_lt> RLDriver<'pattern_lt> {
-    pub fn build(&mut self, font_file: String, rangoli_pattern: &'pattern_lt Vec<String>) -> RLDriver<'pattern_lt> {
+    pub fn build(
+        font_file: String,
+        rangoli_pattern: &'pattern_lt Vec<String>,
+    ) -> RLDriver<'pattern_lt> {
         let (mut rl, thread) = raylib::init()
             .size(SCREEN_WIDTH, SCREEN_HEIGHT)
-            .title("Rust Alphabet Rangoli (ver. 0.9)")
+            .title("Rust Alphabet Rangoli (ver. 0.92)")
             .build();
 
         // Raylib-Rust calls Raylib-C using FFI. When font loading fails, the following Rust code
-        // does not execute the expect() statement. Instead, the C-library handles the fault internally: 
-        // it prints a warning message then falls back on a default font. (See the TRACELOG message 
-        // in LoadBMFont() at rtext.c.) In effect, Rust doesn't panic and abort on account of 
+        // does not execute the expect() statement. Instead, the C-library handles the fault internally:
+        // it prints a warning message then falls back on a default font. (See the TRACELOG message
+        // in LoadBMFont() at rtext.c.) In effect, Rust doesn't panic and abort on account of
         // a failure to load fonts.
 
-        let font = rl.load_font(&thread, font_file.as_str()).expect("Error loading font data.");
+        let font = rl
+            .load_font(&thread, font_file.as_str())
+            .expect("Error loading font data.");
 
-        // TO-DO: Using the font set, calculate the alpha_pos values.
+        // TO-DO: Using the font set, calculate the alpha_offset values.
 
-        RLDriver{
-           rl,
-           thread,
-           font,
-           fps: DEFAULT_FPS,
-           rangoli_pattern,
-           alpha_pos: Default::default(),
-           //alpha_pos: alpha_pos,
+        let mut rangoli_pos = vec![Vector2::default(); rangoli_pattern.len()];
+        for i in 0..rangoli_pattern.len() {
+            rangoli_pos[i].x = (SCREEN_WIDTH as f32 / 2.0)
+                - (rl.measure_text(&rangoli_pattern[i], FONT_SIZE) / 2) as f32;
+            rangoli_pos[i].y = 60.0 + (15.0 * (i as f32));
+        }
+
+        RLDriver {
+            rl,
+            thread,
+            font,
+            fps: DEFAULT_FPS,
+            rangoli_pattern,
+            alpha_offset: Default::default(),
+            rangoli_pos: rangoli_pos,
         }
     }
 
+    fn calc_alpha_offset() {}
 
     pub fn run(&mut self) {
         while !self.rl.window_should_close() {
-
             // *******************
             // Raylib logic block.
             // *******************
@@ -64,14 +79,14 @@ impl<'pattern_lt> RLDriver<'pattern_lt> {
             for i in 0..self.rangoli_pattern.len() {
                 let r_line = self.rangoli_pattern.get(i).unwrap();
                 d.draw_text_ex(
-                    &self.font, 
-                    r_line, 
-                    self.alpha_pos[i as usize],
-                    20.0, 
-                    1.0, 
-                    Color::GRAY
+                    &self.font,
+                    r_line,
+                    self.rangoli_pos[i as usize],
+                    FONT_SIZE as f32,
+                    1.0,
+                    Color::GRAY,
                 );
             }
-        }    
+        }
     }
 }
