32d1740430f840deaac6f67c7d73729fc4f96e4c
Dennis Chua <dmcvocation@gmail.com>
2025-01-03 13:24:52 -0600

    Put in RLDriver::calc_alpha_offset() to caculate the x-axis offset of each of the 26 characters in a given font set.

--- raylib-alpha-rangoli/src/raylib_mod.rs
+++ raylib-alpha-rangoli/src/raylib_mod.rs
@@ -1,10 +1,12 @@
 #![allow(warnings)]
 
 use raylib::prelude::*;
+use std::collections::HashMap;
 
 pub const SCREEN_WIDTH: i32 = 800;
 pub const SCREEN_HEIGHT: i32 = 450;
 pub const DEFAULT_FPS: u32 = 24;
+pub const FONT_SIZE: i32 = 20;
 
 pub struct RLDriver<'pattern_lt> {
     rl: &'pattern_lt mut RaylibHandle,
@@ -13,9 +15,10 @@ pub struct RLDriver<'pattern_lt> {
     font: &'pattern_lt Font,
     // Generated text pattern owned by the rangoli module.
     rangoli_pattern: &'pattern_lt Vec<String>,     
+    // X- and Y-coordinate of each character in the rangoli pattern.
     rangoli_pos: &'pattern_lt Vec<Vector2>,
-    // Relative x- and y-offset of each English alphabet character based on the font set.
-    alpha_pos: [Vector2; 26],  
+    // X-offset of a character of the given font set.
+    alpha_offsets: HashMap<char, f32>,
 }
 
 impl<'pattern_lt> RLDriver<'pattern_lt> {
@@ -23,7 +26,8 @@ impl<'pattern_lt> RLDriver<'pattern_lt> {
                  font: &'pattern_lt Font, rangoli_pattern: &'pattern_lt Vec<String>, 
                  rangoli_pos: &'pattern_lt Vec<Vector2>) -> RLDriver<'pattern_lt> {
 
-        // TO-DO: Using the font set, calculate the alpha_pos values.
+        let alpha_offsets = RLDriver::calc_alpha_offsets(&rl);
+        println!("\n{:#?}", alpha_offsets);
 
         RLDriver{
            rl,
@@ -32,9 +36,28 @@ impl<'pattern_lt> RLDriver<'pattern_lt> {
            fps: DEFAULT_FPS,
            rangoli_pattern,
            rangoli_pos,
-           alpha_pos: Default::default(),
-           //alpha_pos: alpha_pos,
+           alpha_offsets,
         }
+    }
+
+    fn calc_alpha_offsets(rl: &RaylibHandle) -> HashMap<char, f32> {
+        let ascii_lower: [char; 26 as usize] = [
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
+            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 
+            'y', 'z',
+        ];
+
+        let mut retVal: HashMap<char, f32> = HashMap::new();
+
+        // A temporary variable is required for a char conversion because 
+        // a char in Rust is a 4 byte UTF8, unlike ASCII.
+        let mut tmp = [0u8; 4];
+        for i in 0..26 {
+            let x_offset = (rl.measure_text(ascii_lower[i].encode_utf8(&mut tmp), FONT_SIZE) / 2) as f32;  
+            retVal.insert(ascii_lower[i], x_offset);
+        }
+
+        retVal
     }
 
 
