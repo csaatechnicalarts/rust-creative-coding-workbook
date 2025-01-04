f17def1c95f9afbdda77290d27405620755006fa
Dennis Chua <dmcvocation@gmail.com>
2025-01-04 06:48:43 -0600

    Adjust y-axis padding to better separate 'i' and 'j' on stacked rows. Clear away rangoli_pos, which has become scaffolding code now that RLDriver.rangoli_disp is in place.

--- raylib-alpha-rangoli/src/main.rs
+++ raylib-alpha-rangoli/src/main.rs
@@ -12,10 +12,10 @@ struct CliParam {
 }
 
 fn main() {
-    use std::process;
+    use crate::rangoli::{generate_rangoli_pattern, print_rangoli, LOWER_BOUND, UPPER_BOUND};
+    use crate::raylib_mod::{RLDriver, DEFAULT_FPS, SCREEN_HEIGHT, SCREEN_WIDTH};
     use raylib::prelude::*;
-    use crate::raylib_mod::{SCREEN_HEIGHT, SCREEN_WIDTH, DEFAULT_FPS, RLDriver};
-    use crate::rangoli::{print_rangoli, generate_rangoli_pattern, LOWER_BOUND, UPPER_BOUND};
+    use std::process;
 
     // *************************
     // Rangoli generation block.
@@ -32,14 +32,12 @@ fn main() {
 
     //print_rangoli(cli_param.number);
 
-    let (rangoli_pattern , max_size) = generate_rangoli_pattern(cli_param.number);
+    let (rangoli_pattern, max_size) = generate_rangoli_pattern(cli_param.number);
     //println!("{:#?}", rangoli_pattern);
-
 
     // ********************
     // Raylib set up block.
     // ********************
-
 
     let (mut rl, thread) = raylib::init()
         .size(SCREEN_WIDTH, SCREEN_HEIGHT)
@@ -50,22 +48,14 @@ fn main() {
 
     // Raylib-Rust calls Raylib-C using FFI. When font loading fails, the following Rust code
     // does not print the expect() message then panic. Instead, internally the C-library falls
-    // back on its default font, after printing a warning message. See rtext.c LoadBMFont() 
+    // back on its default font, after printing a warning message. See rtext.c LoadBMFont()
     // TRACELOG message.
- 
-    let font = rl.load_font(&thread, "resources/alpha_beta.png").expect("Couldn't load font!");
+
+    let font = rl
+        .load_font(&thread, "resources/bad_alpha_beta.png")
+        .expect("Couldn't load font!");
     //println!("\n{:#?}", font);
 
-    let mut rangoli_pos = vec![Vector2::default(); rangoli_pattern.len()];
-
-    for i in 0..rangoli_pattern.len() {
-        rangoli_pos[i].x = (SCREEN_WIDTH as f32 / 2.0) - (rl.measure_text(&rangoli_pattern[i], 20)/2) as f32;
-        rangoli_pos[i].y = 60.0 + (15.0 * (i as f32));
-    }
-    //println!("\n{:#?}", rangoli_pos);
-
-    let mut rld = RLDriver::build(&mut rl, &thread, &font, &rangoli_pattern, &rangoli_pos);
+    let mut rld = RLDriver::build(&mut rl, &thread, &font, &rangoli_pattern);
     rld.run();
-
 }
-
--- raylib-alpha-rangoli/src/raylib_mod.rs
+++ raylib-alpha-rangoli/src/raylib_mod.rs
@@ -10,12 +10,12 @@ pub const FONT_SIZE: f32 = 18.0;
 
 const ALPHA_DELIM: char = '-';
 const ALPHA_WIDTH_PAD: f32 = 3.0;
-const ALPHA_HEIGHT_PAD: f32 = 16.5;
+const ALPHA_HEIGHT_PAD: f32 = 18.5;
 
 #[derive(Debug)]
 struct AlphaToDisplay {
     alpha: char,
-    coord: Vector2
+    coord: Vector2,
 }
 
 pub struct RLDriver<'pattern_lt> {
@@ -24,38 +24,41 @@ pub struct RLDriver<'pattern_lt> {
     fps: u32,
     font: &'pattern_lt Font,
     // Generated text pattern owned by the rangoli module.
-    rangoli_pattern: &'pattern_lt Vec<String>,     
-    // X- and Y-coordinate of each character in the rangoli pattern.
-    rangoli_pos: &'pattern_lt Vec<Vector2>,
+    rangoli_pattern: &'pattern_lt Vec<String>,
     rangoli_disp: Vec<Vec<AlphaToDisplay>>,
     // X-offset of a character of the given font set.
     alpha_offsets: HashMap<char, f32>,
 }
 
 impl AlphaToDisplay {
-    fn new(c: char, line_index: usize, mid_index: usize, char_index: usize, 
-           alpha_offsets: &HashMap<char, f32>, max_alpha_offset: f32) -> AlphaToDisplay {
+    fn new(
+        c: char,
+        line_index: usize,
+        mid_index: usize,
+        char_index: usize,
+        alpha_offsets: &HashMap<char, f32>,
+        max_alpha_offset: f32,
+    ) -> AlphaToDisplay {
         let mut alpha_coord = Vector2::default();
 
         if char_index == mid_index {
             alpha_coord.x = SCREEN_WIDTH as f32 / 2.0;
         } else if char_index > mid_index {
-            alpha_coord.x = (SCREEN_WIDTH as f32 / 2.0) + 
-                ((char_index - mid_index) as f32 * (max_alpha_offset + 2.0)) + 
-                ((char_index - mid_index) as f32 * ALPHA_WIDTH_PAD);
-                //alpha_offsets.get(&c).unwrap();
+            alpha_coord.x = (SCREEN_WIDTH as f32 / 2.0)
+                + ((char_index - mid_index) as f32 * (max_alpha_offset + 2.0))
+                + ((char_index - mid_index) as f32 * ALPHA_WIDTH_PAD);
+            //alpha_offsets.get(&c).unwrap();
         } else {
-            alpha_coord.x = (SCREEN_WIDTH as f32 / 2.0) - 
-                ((mid_index - char_index) as f32 * (max_alpha_offset + 2.0)) - 
-                ((mid_index - char_index) as f32 * ALPHA_WIDTH_PAD);
-                //alpha_offsets.get(&c).unwrap();
-
+            alpha_coord.x = (SCREEN_WIDTH as f32 / 2.0)
+                - ((mid_index - char_index) as f32 * (max_alpha_offset + 2.0))
+                - ((mid_index - char_index) as f32 * ALPHA_WIDTH_PAD);
+            //alpha_offsets.get(&c).unwrap();
         }
         alpha_coord.y = 60.0 + (ALPHA_HEIGHT_PAD * (line_index as f32));
 
         let mut retVal = AlphaToDisplay {
             alpha: c,
-            coord: alpha_coord
+            coord: alpha_coord,
         };
 
         retVal
@@ -63,12 +66,12 @@ impl AlphaToDisplay {
 }
 
 impl<'pattern_lt> RLDriver<'pattern_lt> {
-    pub fn build(rl: &'pattern_lt mut RaylibHandle, 
-        thread: &'pattern_lt RaylibThread, 
-        font: &'pattern_lt Font, 
-        rangoli_pattern: &'pattern_lt Vec<String>, 
-        rangoli_pos: &'pattern_lt Vec<Vector2>) -> RLDriver<'pattern_lt> {
-
+    pub fn build(
+        rl: &'pattern_lt mut RaylibHandle,
+        thread: &'pattern_lt RaylibThread,
+        font: &'pattern_lt Font,
+        rangoli_pattern: &'pattern_lt Vec<String>,
+    ) -> RLDriver<'pattern_lt> {
         let (max_alpha_offset, alpha_offsets) = RLDriver::calc_alpha_offsets(&rl);
         //println!("\n{:#?}\nmax_alpha_offset: {:.2}", alpha_offsets, max_alpha_offset);
 
@@ -79,12 +82,11 @@ impl<'pattern_lt> RLDriver<'pattern_lt> {
             let mut inner_vec: Vec<AlphaToDisplay> = Vec::new();
 
             // The middle character is always 'a', the pivot of the range of characters
-            // incrementing to the left and right respectively: 
+            // incrementing to the left and right respectively:
             // i.e. ["a", "b-a-b", "c-b-a-b-c"] for an n=3 rangoli pattern.
-            
+
             let mid_index: usize = r_line.len() / 2;
             for char_index in 0..r_line.len() {
-
                 // A line in a rangoli pattern always has an odd number of alphabets,
                 // each one indexed by an even number. Odd indices always refer to
                 // the delimeter character, '-' in "c-b-a-b-c" for example.
@@ -92,17 +94,16 @@ impl<'pattern_lt> RLDriver<'pattern_lt> {
                 // We could have done the following, but modulo arithmetic is always
                 // faster than a memory lookup and logical comparison combo operation:
                 // if r_line.chars().nth(j as usize) != Some(ALPHA_DELIM) {...}
-                
+
                 if char_index % 2 == 0 {
                     inner_vec.push(AlphaToDisplay::new(
-                            r_line.chars().nth(char_index).unwrap(),
-                            line_index,
-                            mid_index,
-                            char_index,
-                            &alpha_offsets,
-                            max_alpha_offset,
-                            )
-                        );
+                        r_line.chars().nth(char_index).unwrap(),
+                        line_index,
+                        mid_index,
+                        char_index,
+                        &alpha_offsets,
+                        max_alpha_offset,
+                    ));
                 }
             }
 
@@ -110,32 +111,32 @@ impl<'pattern_lt> RLDriver<'pattern_lt> {
         }
         println!("{:#?}", outer_vec);
 
-        RLDriver{
-           rl,
-           thread,
-           font,
-           fps: DEFAULT_FPS,
-           rangoli_pattern,
-           rangoli_pos,
-           rangoli_disp: outer_vec,
-           alpha_offsets,
+        RLDriver {
+            rl,
+            thread,
+            font,
+            fps: DEFAULT_FPS,
+            rangoli_pattern,
+            rangoli_disp: outer_vec,
+            alpha_offsets,
         }
     }
 
     fn calc_alpha_offsets(rl: &RaylibHandle) -> (f32, HashMap<char, f32>) {
         let ascii_lower: [char; 26 as usize] = [
-            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
-            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 
-            'y', 'z',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+            'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
         ];
         let mut ret_val: HashMap<char, f32> = HashMap::new();
         let mut max_alpha_offset: f32 = 0.0;
 
-        // A temporary variable is required for a char conversion because 
+        // A temporary variable is required for a char conversion because
         // a char in Rust is a 4 byte UTF8, unlike ASCII.
+
         let mut tmp = [0u8; 4];
         for i in 0..26 {
-            let x_offset = (rl.measure_text(ascii_lower[i].encode_utf8(&mut tmp), FONT_SIZE as i32) / 2) as f32;  
+            let x_offset = (rl.measure_text(ascii_lower[i].encode_utf8(&mut tmp), FONT_SIZE as i32)
+                / 2) as f32;
             ret_val.insert(ascii_lower[i], x_offset);
             if x_offset > max_alpha_offset {
                 max_alpha_offset = x_offset;
@@ -145,10 +146,8 @@ impl<'pattern_lt> RLDriver<'pattern_lt> {
         (max_alpha_offset, ret_val)
     }
 
-
     pub fn run(&mut self) {
         while !self.rl.window_should_close() {
-
             // *******************
             // Raylib logic block.
             // *******************
@@ -160,20 +159,7 @@ impl<'pattern_lt> RLDriver<'pattern_lt> {
             let mut d = self.rl.begin_drawing(&self.thread);
 
             d.clear_background(Color::GAINSBORO);
-/*
-            for i in 0..self.rangoli_pattern.len() {
-                let r_line = self.rangoli_pattern.get(i).unwrap();
-                d.draw_text_ex(
-                    &self.font, 
-                    r_line, 
-                    self.rangoli_pos[i as usize],
-                    FONT_SIZE, 
-                    1.0, 
-                    Color::GRAY
-                );
-            }
-*/
-            
+
             for r_line in &self.rangoli_disp {
                 for r_char in r_line {
                     let mut tmp = [0u8; 4];
@@ -184,11 +170,10 @@ impl<'pattern_lt> RLDriver<'pattern_lt> {
                         r_char.coord,
                         FONT_SIZE,
                         1.0,
-                        Color::GRAY
+                        Color::GRAY,
                     );
                 }
             }
-
-        }    
+        }
     }
 }
