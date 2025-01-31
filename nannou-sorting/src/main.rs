f711a90737000ffb4b81486ad3fd9fed85c90d86
Dennis Chua <dmcvocation@gmail.com>
2025-01-31 10:37:21 -0600

    Use bubble sort module in crate main file.

--- nannou-sorting/src/main.rs
+++ nannou-sorting/src/main.rs
@@ -1,3 +1,12 @@
+use crate::bubble_sort::*;
+
+pub mod bubble_sort;
+
 fn main() {
-    println!("Hello, world!");
+    let mut v = vec![2, 13, 4, 7, 8, 1, 5];
+
+    bubble_sort(&mut v);
+
+    assert_eq!(v, vec![1, 2, 4, 5, 7, 8, 13]);
+    println!("{v:?}");
 }
