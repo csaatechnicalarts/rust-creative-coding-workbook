eadbdf1d414ab2f305a63429f42eaccd83d2c097
Dennis Chua <dmcvocation@gmail.com>
2025-01-31 12:21:06 -0600

    Put in basic BubbleSort structure, implementing new() and fmt::Display().
    TODO: Handle case when an empty vector is passed to BubbleSort. The method new() should return Option<Self, Err> of some sort.

--- nannou-sorting/src/bubble_sort.rs
+++ nannou-sorting/src/bubble_sort.rs
@@ -1,8 +1,44 @@
 use std::fmt::Debug;
+use std::fmt;
+
+struct BubbleSort<'a, T: PartialOrd + Debug> {
+    v: &'a mut Vec<T>,
+    outer_max: u32,
+    outer_idx: u32,
+    inner_idx: u32,
+}
+
+impl<'a, T> BubbleSort<'a, T>
+where T: PartialOrd + Debug 
+{
+    fn new(v: &'a mut Vec<T>) -> Self {
+        let v_len = v.len() as u32;
+        let ret_val = BubbleSort {
+            v,
+            outer_max: v_len,
+            outer_idx: 0,
+            inner_idx: 0,
+        };
+
+        ret_val
+    }
+}
+
+impl<'a, T> fmt::Display for BubbleSort<'a, T>
+where T: PartialOrd + Debug
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:#?}\nouter_max: {}\nouter_idx: {}\ninner_idx: {}\n",
+               self.v,
+               self.outer_max,
+               self.outer_idx,
+               self.inner_idx)
+    }
+}
 
 // Big-O(n^2)
 // Visualization - https://www.hackerearth.com/practice/algorithms/sorting/bubble-sort/visualize/
-pub fn bubble_sort <T: PartialOrd + Debug> (v: &mut [T]) {
+pub fn proto_bubble_sort <T: PartialOrd + Debug> (v: &mut [T]) {
     for p in 0..v.len() {
         let mut sorted = true;
         for i in 0..(v.len() - 1) - p {
@@ -24,17 +60,37 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_bubble_sort() {
+    fn test_proto_bubble_sort() {
         let mut v = vec![2, 13, 4, 7, 8, 1, 5];
-        bubble_sort(&mut v);
+        proto_bubble_sort(&mut v);
         assert_eq!(v, vec![1, 2, 4, 5, 7, 8, 13]);
     }
 
-    #[ignore]
     #[test]
-    fn test_bubble_sort_contra() {
+    fn test_proto_bubble_sort_contra() {
         let mut a = vec!['a', 'x', 'm', 'n', 'h', 'c'];
-        bubble_sort(&mut a);
+        proto_bubble_sort(&mut a);
         assert_ne!(a, vec!['a']);
+    }
+
+    #[test]
+    fn test_bubble_sort_constructor() {
+        /*let mut v0: Vec<u32> = Vec::new();
+        let bs0 = BubbleSort::new(&mut v0);
+
+        assert_eq!(bs0.v.get(0), None);
+        assert_eq!(bs0.outer_max, 0);*/
+
+        let mut v1 = vec![4, 2, 1];
+        let bs1 = BubbleSort::new(&mut v1);
+
+        assert_eq!(bs1.v.get(0), Some(&4));
+        assert_eq!(bs1.v.get(1), Some(&2));
+        assert_eq!(bs1.v.get(2), Some(&1));
+        assert_eq!(bs1.v.get(3), None);
+
+        assert_eq!(bs1.outer_max, 3);
+        assert_eq!(bs1.outer_idx, 0);
+        assert_eq!(bs1.inner_idx, 0);
     }
 }
--- nannou-sorting/src/main.rs
+++ nannou-sorting/src/main.rs
@@ -5,7 +5,7 @@ pub mod bubble_sort;
 fn main() {
     let mut v = vec![2, 13, 4, 7, 8, 1, 5];
 
-    bubble_sort(&mut v);
+    proto_bubble_sort(&mut v);
 
     assert_eq!(v, vec![1, 2, 4, 5, 7, 8, 13]);
     println!("{v:?}");
