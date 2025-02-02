145a5044e68f3402027e0d3c1ef041a29d74fafe
Dennis Chua <dmcvocation@gmail.com>
2025-02-01 18:07:43 -0600

    Remove duplicate call to bubble_sort.algo_next().

--- nannou-sorting/src/main.rs
+++ nannou-sorting/src/main.rs
@@ -6,13 +6,6 @@ fn main() {
     let mut v = vec![2, 13, 4, 7, 8, 1, 5];
     let mut w = v.clone();
     println!("Unsorted vector\t\t{:?}", v);
-    loop {
-        if bubble_sort.algo_next() == true {
-            println!("Step-wise bubble sort\t{:?}", bubble_sort.get_vec());
-
-            break;
-        }
-    }
 
     proto_bubble_sort(&mut v);
     assert_eq!(v, vec![1, 2, 4, 5, 7, 8, 13]);
