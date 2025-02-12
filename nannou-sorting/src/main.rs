8e754eec85e41e8e3c1a0584c752a582e39a62f3
Dennis Chua <dmcvocation@gmail.com>
2025-02-12 12:05:21 -0600

    feat: Replace hard-coded data to sort with data stream read from stdin.

--- nannou-sorting/src/main.rs
+++ nannou-sorting/src/main.rs
@@ -1,36 +1,40 @@
-use std::process;
-use crate::bubble_sort::{proto_bubble_sort, BubbleSort};
+use std::{io, process};
+use crate::bubble_sort::BubbleSort;
 
 pub mod bubble_sort;
 
-fn main() {
-    let mut v = vec![2, 13, 4, 7, 8, 1, 5, 10, 11, 3];
-    let mut w = v.clone();
-    println!("Unsorted vector\t\t{:?}", v);
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut input_str = String::new();
+    let mut str_int: u32;
+    let mut data_vec = Vec::<u32>::new();
 
-    proto_bubble_sort(&mut v);
-    println!("Algorithmic bubble sort\t{v:?}");
+    while io::stdin().read_line(&mut input_str)? != 0 {
+        str_int = input_str.trim().parse::<u32>().unwrap_or_else(|err| {
+            println!("{}: {:?}", err, input_str);
 
-    let mut bubble_sort: BubbleSort<u32>;
-    let bs = BubbleSort::new(&mut w);
+            0
+        });
 
-    match bs {
-        Ok(bubble_sort_struct) => {
-            bubble_sort = bubble_sort_struct;
-        },
-        Err(err) => {
-            println!("[Error]: {} Good-bye!", err);
-            process::exit(1);
-        }
-
+        data_vec.push(str_int);
+        input_str.clear();
     }
+    println!("Original input:\t\t{:?}", data_vec);
+
+    let mut bubble_sort: BubbleSort<u32> = BubbleSort::new(&mut data_vec)
+        .unwrap_or_else(|err| {
+            println!("[Error]: {} Good-bye!", err);
+
+            process::exit(1);
+        });
 
     loop {
         bubble_sort.algo_next();
         if bubble_sort.is_sorted() == true {
-            println!("Step-wise bubble sort\t{:?}", bubble_sort.get_vec());
+            println!("Step-wise bubble sort:\t{:?}", bubble_sort.get_vec());
 
             break;
         }
     }
+
+    Ok(())
 }
