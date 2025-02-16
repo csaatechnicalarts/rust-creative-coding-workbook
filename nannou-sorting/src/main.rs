2215f6c47611fd914e371c0c41119ab0cce2890a
Dennis Chua <dmcvocation@gmail.com>
2025-02-16 14:19:25 -0600

    update: Put in comment docs for main() function.

--- nannou-sorting/src/main.rs
+++ nannou-sorting/src/main.rs
@@ -3,6 +3,20 @@ use crate::bubble_sort::BubbleSort;
 
 pub mod bubble_sort;
 
+/// The main function for the nannou-sorting binary. The program expect a stream of
+/// integers from stdin which it, in turn, sorts and prints out. In the absence of
+/// data piped in the command line, as shown in the example below, nannou-sorting
+/// enters interactive mode, accepting integer inputs separated by new line until
+/// it reads the EOF (Ctr-D) character.
+///
+/// # Example Usage:
+///
+/// ```
+/// $> shuf -i 1-100 -n 10 | ./nannou-sorting
+/// Original input:		    [38, 51, 23, 46, 33, 96, 94, 17, 73, 54]
+/// Step-wise bubble sort:	[17, 23, 33, 38, 46, 51, 54, 73, 94, 96]
+/// ```
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut input_str = String::new();
     let mut str_int: u32;
